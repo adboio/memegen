b559da19b06de3029b996899c3e5675979c80202
Adam Bowker <adambowker98@gmail.com>
2025-04-01 22:04:22 -0700

    _actually_ make templates efficient...

--- app/helpers.py
+++ app/helpers.py
@@ -1,32 +1,63 @@
 from sanic import Request
+import time
+import os
+from pathlib import Path
 
 from . import settings, utils
 from .models import Template
 
+_template_cache = {
+    'templates': None,
+    'last_reload': None
+}
+
+def should_reload_templates():
+    """Check if any template files have been modified since last reload"""
+    if _template_cache['last_reload'] is None:
+        return True
+        
+    template_dir = Path("path/to/your/templates")
+    latest_mtime = max(
+        os.path.getmtime(f) 
+        for f in template_dir.rglob('*') 
+        if f.is_file()
+    )
+    
+    return latest_mtime > _template_cache['last_reload']
+
+def load_templates():
+    """Actually load templates from disk"""
+    templates = Template.objects.filter(valid=True, _exclude="_custom")
+    return sorted(templates)
+
+def get_cached_templates(force_reload=False):
+    """Get templates from cache, reloading if needed"""
+    if _template_cache['templates'] is None or force_reload:
+        _template_cache['templates'] = load_templates()
+        _template_cache['last_reload'] = time.time()
+    return _template_cache['templates']
 
 def get_valid_templates(
     request: Request, query: str = "", animated: bool | None = None
 ) -> list[dict]:
-    templates: list[Template] = Template.objects.filter(valid=True, _exclude="_custom")
+    templates = get_cached_templates()
+    
     if query:
         templates = [t for t in templates if t.matches(query)]
-    else:
-        templates = sorted(templates)
     if animated is True:
         templates = [t for t in templates if "animated" in t.styles]
     elif animated is False:
         templates = [t for t in templates if "animated" not in t.styles]
+    
     return [template.jsonify(request) for template in templates]
-
 
 def get_example_images(
     request: Request, query: str = "", *, animated: bool | None = None
 ) -> list[tuple[str, str]]:
-    templates = Template.objects.filter(valid=True, _exclude="_custom")
+    templates = get_cached_templates()
+    
     if query:
         templates = [t for t in templates if t.matches(query)]
-    else:
-        templates = sorted(templates)
 
     if animated is None:
         animated = utils.urls.flag(request, "animated")
@@ -53,7 +84,6 @@ def get_example_images(
         images.append((example, self))
 
     return images
-
 
 def get_test_images(request: Request) -> list[str]:
     animated = utils.urls.flag(request, "animated")
